be87838df9c9afb123533c2bcd8deea56c296e17
Andrey-Legion <113177811+Andrey-Legion@users.noreply.github.com>
2023-01-08 14:43:00 +0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      int daysPast=234;
+      int holeWeek=daysPast/7, holeMonth=daysPast/31;
+      System.out.println("Прошло недель- "+ holeWeek+"\n"+"Прошло месяцев- "+holeMonth);
+  }
+}
